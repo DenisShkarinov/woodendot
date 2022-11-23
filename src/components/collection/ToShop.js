bcdf780d4ca442b4980c009581a8dc6479dd41e6
Denis <dskarinov@gmail.com>
2022-11-23 11:02:26 +0300

    Update ToShop.js

--- src/components/collection/ToShop.js
+++ src/components/collection/ToShop.js
@@ -2,7 +2,7 @@
 function ToShop (props) {
    return (
       <div className="description-link">
-         <button id="shop" aria-label={props.toShop}><a href="#" aria-label="To shop">{props.toShop}</a></button>
+         <button aria-label={props.toShop}><a href="#" aria-label="To shop">{props.toShop}</a></button>
       </div>
    )
 }
