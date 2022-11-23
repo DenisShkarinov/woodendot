6e9e85b8c4578943e015f6dbb15ff8c634e9576e
Denis <dskarinov@gmail.com>
2022-11-23 22:33:04 +0300

    Update Link.js

--- src/components/header/Link.js
+++ src/components/header/Link.js
@@ -1,7 +1,8 @@
 function Link (props) {
+   
    return (
       <div className={props.class}> 
-         <a href="#" aria-label={props.linkTitle !== null ? "Go to page " + props.linkTitle : "Go to page Homepage"}>{props.linkTitle || <img src={props.imgSrc} alt="" /> }</a>
+         <a href="#" aria-label={props.linkTitle ?? "Go to Homepage"}>{props.linkTitle ?? <img src={props.imgSrc} alt="Woodendot" /> }</a>
       </div>
    )
 }
