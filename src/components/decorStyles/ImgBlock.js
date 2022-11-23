35296b2e89469779456f1c4da8b1582b2f631468
Denis <dskarinov@gmail.com>
2022-11-23 11:04:01 +0300

    Update ImgBlock.js

--- src/components/decorStyles/ImgBlock.js
+++ src/components/decorStyles/ImgBlock.js
@@ -4,7 +4,7 @@ function ImgBlock (props) {
       <div className="img-block">
          <img src={props.src} alt={props.title} />
          <div className="overlay">
-            <button id="more" aria-label="See more">See More</button>
+            <button aria-label="See more">See More</button>
          </div>
       </div>
    )
